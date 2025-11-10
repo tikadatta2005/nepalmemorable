3f8435c4beed9dc7e070f8db1bfbd42ed20bfc20
Deepsu Gautam <tikadatta2005@gmail.com>
2025-11-10 12:21:44 +0545

    Changes

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,6 @@
 import Banner3 from "@/components/reusables/banners/Banner3";
 import Card1 from "@/components/reusables/cards/Card1";
+import BasicHolder from "@/components/reusables/holder/BasicHolder";
 import { GetData } from "@/utils/GetData";
 import React from "react";
 
@@ -8,7 +9,7 @@ export const dynamic = "force-dynamic";
 
 const Services = async () => {
   const res = await GetData(
-    "/get-contents?type=services&sort=1&page=0"
+    "/get-contents?type=services&sort=-1&page=0&len=20"
   );
 
   const basicData = [
@@ -59,7 +60,7 @@ const Services = async () => {
         title="Explore Nepal – Treks & Tours of a Lifetime"
         description="Discover the beauty of Nepal with Nepal Memorable. From breathtaking Himalayan treks to cultural tours, we craft unforgettable adventures that let you experience nature, tradition, and adventure all in one journey."
       />
-      <section className="w-full max-w-7xl mx-auto p-4 py-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <BasicHolder Card={Card1} initial={1} url='/get-contents?type=services&sort=-1&page:page0&len=20' className="w-full max-w-7xl mx-auto p-4 py-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {data?.map((elem, index) => {
           return (
             <Card1
@@ -75,7 +76,7 @@ const Services = async () => {
             />
           );
         })}
-      </section>
+      </BasicHolder>
     </main>
   );
 };
