13f5cb5c7aa19899900d4155653ee301f0519c80
Deepsu Gautam <tikadatta2005@gmail.com>
2025-11-11 20:52:19 +0545

    Change in design

--- components/Home/Trips.jsx
+++ components/Home/Trips.jsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { NextWhite, PrevWhite } from "../reusables/buttons/CarouselButtons";
 import { GetData } from "@/utils/GetData";
 import { ChevronRight } from "lucide-react";
+import CsrImage from "../reusables/assets/CsrImage";
+import CsrBg from "../reusables/assets/CsrBg";
 
 const Trips = async () => {
   const res = await GetData("/get-contents?type=tours&page=0&sort=-1&len=6");
@@ -45,43 +47,48 @@ const Trips = async () => {
   };
 
   return (
-    <section className="w-full relative">
-      <div className="w-full bg-linear-to-b from-cyan-700 to-cyan-600 pb-20 pt-20 p-4">
-        <div className="w-full max-w-7xl mx-auto p-4">
-          <div className="w-full flex justify-between">
-            <div className="flex w-fit flex-col gap-2">
-              <b className="text-amber-200 w-fit text-sm md:text-base">
-                POPULAR TRIPS
-              </b>
-              <h2 className="text-white font-pacifico text-2xl md:text-3xl lg:text-4xl leading-tight mb-6">
-                Tour In Nepal
-              </h2>
+    <section className="w-full relative overflow-hidden">
+      <CsrBg
+        className="bg-center bg-fixed bg-cover"
+        style={{ backgroundImage: `url("/assets/covers/lake.jpg")` }}
+      >
+        <div className="w-full relative z-10 pb-20 pt-20 p-4 bg-black/20">
+          <div className="w-full max-w-7xl mx-auto p-4">
+            <div className="w-full flex justify-between">
+              <div className="flex w-fit flex-col gap-2">
+                <b className="text-amber-200 w-fit text-sm md:text-base text-shadow-2xs">
+                  POPULAR TRIPS
+                </b>
+                <h2 className="text-white font-pacifico text-2xl md:text-3xl lg:text-4xl leading-tight mb-6">
+                  Tour In Nepal
+                </h2>
+              </div>
+              <div className="w-fit flex flex-col justify-end py-4">
+                <Link href="/services/tours" className="w-fit h-fit">
+                  <button className="w-fit md:text-base flex gap-2 items-center text-sm font-medium text-white p-2 px-4 rounded-lg bg-amber-500 transition-all duration-300 hover:shadow-xl hover:scale-105 cursor-pointer">
+                    See all
+                    <ChevronRight />
+                  </button>
+                </Link>
+              </div>
             </div>
-            <div className="w-fit flex flex-col justify-end py-4">
-              <Link href="/services/tours" className="w-fit h-fit">
-                <button className="w-fit md:text-base flex gap-2 items-center text-sm font-medium text-white p-2 px-4 rounded-lg bg-amber-500 transition-all duration-300 hover:shadow-xl hover:scale-105 cursor-pointer">
-                  See all
-                  <ChevronRight />
-                </button>
-              </Link>
-            </div>
+            {/* div of react slider */}
+            <SlickCarousel
+              settings={settings}
+              PrevButton={PrevWhite}
+              NextButton={NextWhite}
+            >
+              {[...data, ...data, ...data]?.map((element, index) => {
+                return (
+                  <div key={index} className="w-full p-1">
+                    <TripCard data={element} />
+                  </div>
+                );
+              })}
+            </SlickCarousel>
           </div>
-          {/* div of react slider */}
-          <SlickCarousel
-            settings={settings}
-            PrevButton={PrevWhite}
-            NextButton={NextWhite}
-          >
-            {[...data, ...data, ...data]?.map((element, index) => {
-              return (
-                <div key={index} className="w-full p-1">
-                  <TripCard data={element} />
-                </div>
-              );
-            })}
-          </SlickCarousel>
         </div>
-      </div>
+      </CsrBg>
     </section>
   );
 };
